65600dba4d469fd1151c010cec5807ef8a914cc9
smseow001 <33817483+smseow001@users.noreply.github.com>
2023-07-28 13:00:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,16 +23,16 @@ git clone https://github.com/thinktip/luci-theme-neobird package/luci-theme-neob
 git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
 
 # Add a feed source
-#sed -i '$a src-git vssr https://github.com/jerrykuku/luci-app-vssr.git' feeds.conf.default
+sed -i '$a src-git vssr https://github.com/jerrykuku/luci-app-vssr.git' feeds.conf.default
 #sed -i '$a src-git maxminddb https://github.com/jerrykuku/lua-maxminddb.git' feeds.conf.default
 
 # AdGuard home
-#sed -i '$a src-git diy https://github.com/rufengsuixing/luci-app-adguardhome.git' feeds.conf.default
+sed -i '$a src-git diy https://github.com/rufengsuixing/luci-app-adguardhome.git' feeds.conf.default
 
 # kenzok8 packages
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/diy/luci-app-vssr  #vssr出国软件
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/diy/luci-app-vssr  #vssr出国软件
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/diy/lua-maxminddb  #git lua-maxminddb 依赖
